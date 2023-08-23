7407dc3fb80d7314a691f1365defafc3470d4223
Eliane Dantas <edantas241@gmail.com>
2023-08-23 20:54:05 -0300

    Implementação da interface IViagemService.

---
+++ Codigo/Core/Service/IViagemService.cs
@@ -0,0 +1,13 @@
+﻿using Core.DTO;
+
+namespace Core.Service
+{
+    public interface IViagemService
+    {
+        int Create(Viagem viagem);
+        Viagem Get(int id);
+
+        IEnumerable<Viagem> GetAll();
+        IEnumerable<ViagemDto> GetByLocalDestino(string localdestino);
+    }
+}
