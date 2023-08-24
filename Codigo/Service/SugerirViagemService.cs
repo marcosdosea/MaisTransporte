5344f294f7f8c3c10aa6fba2084cd75cf98c2c08
nataliascosta <102239679+nataliascosta@users.noreply.github.com>
2023-08-23 22:22:42 -0300

    Criação da classe SugerirViagemService

---
+++ Codigo/Service/SugerirViagemService.cs
@@ -0,0 +1,39 @@
+﻿using Core;
+using Core.DTO;
+using Core.Service;
+
+namespace Service
+{
+    public class SugerirViagemService : ISugerirViagemService
+    {
+        private readonly MaisTransporteContext _context;
+
+        public SugerirViagemService(MaisTransporteContext context)
+        {
+            _context = context;
+        }
+
+        public int Create(Sugestaoviagem sugestaoviagem)
+        {
+            _context.Add(sugestaoviagem);
+            _context.SaveChanges();
+            return sugestaoviagem.Id;
+        }
+
+        public Sugestaoviagem Get(int id)
+        {
+            return _context.Sugestaoviagems.Find(id);
+        }
+
+        public IEnumerable<Sugestaoviagem> GetAll()
+        {
+            return _context.Sugestaoviagems;
+        }
+
+        public IEnumerable<SugestaoViagemDto> GetByLocalDestino(string localDestino)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
