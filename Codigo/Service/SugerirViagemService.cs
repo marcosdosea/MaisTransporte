285da21c79f372a4163c6a317e96181b40366668
Eliane Dantas <edantas241@gmail.com>
2023-08-24 21:20:22 -0300

    Atualização da classe SugestaoViagemService.

--- Codigo/Service/SugerirViagemService.cs
+++ Codigo/Service/SugerirViagemService.cs
@@ -32,7 +32,15 @@ namespace Service
 
         public IEnumerable<SugestaoViagemDto> GetByLocalDestino(string localDestino)
         {
-            throw new NotImplementedException();
+            IQueryable<Sugestaoviagem> tb_sugestaoViagem = _context.Sugestaoviagems;
+            var query = from sugestaoviagem in tb_sugestaoViagem
+                        where localDestino.StartsWith(localDestino)
+                        orderby sugestaoviagem.LocalDestino descending
+                        select new SugestaoViagemDto
+                        {
+                            LocalDestino = sugestaoviagem.LocalDestino
+                        };
+            return query;
         }
 
     }
