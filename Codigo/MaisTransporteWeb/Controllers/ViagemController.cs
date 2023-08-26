2f1363bc781b8efa6ecb1cf2006ae641a29c72b8
Eliane Dantas <edantas241@gmail.com>
2023-08-26 16:40:14 -0300

    Criação da classe ViagemController

---
+++ Codigo/MaisTransporteWeb/Controllers/ViagemController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Core.Service;
+using Core;
+using MaisTransporteWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaisTransporteWeb.Controllers
+{
+    public class ViagemController : Controller
+    {
+        private readonly IViagemService viagemService;
+        private readonly IMapper mapper;
+
+        public ViagemController(IViagemService viagemService, IMapper mapper)
+        {
+            this.viagemService = viagemService;
+            this.mapper = mapper;
+        }
+
+        public ActionResult Index()
+        {
+            var listaViagens = viagemService.GetAll();
+            var listaViagemViewModel = mapper.Map<List<ViagemViewModel>>(listaViagens);
+            return View(listaViagemViewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var viagem = viagemService.Get(id);
+            var viagemViewModel = mapper.Map<ViagemViewModel>(viagem);
+            return View(viagemViewModel);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
