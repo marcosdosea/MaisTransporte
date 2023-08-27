3108265402e884de24ef048e8c0faf63f04826a5
Eliane Dantas <edantas241@gmail.com>
2023-08-27 00:15:21 -0300

    Injeção de dependências (classes concretas - Base de dados, Mapper e Services)

--- Codigo/MaisTransporteWeb/Program.cs
+++ Codigo/MaisTransporteWeb/Program.cs
@@ -1,27 +1,54 @@
-var builder = WebApplication.CreateBuilder(args);
+using Core;
+using Core.Service;
+using Service;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace BibliotecaWeb
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<MaisTransporteContext>(
+                options => options.UseMySQL(builder.Configuration.GetConnectionString("MaisTransporteDatabase")));
+
+            //injeção dependência Services
+            builder.Services.AddTransient<IViagemService, ViagemService>();
+            builder.Services.AddTransient<ISugerirViagemService, SugerirViagemService>();
+            //builder.Services.AddTransient<IUsuarioService, UsuarioService>();
+            //builder.Services.AddTransient<IVeiculoService, VeiculoService>();
+
+            // injeção dependência mappers
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
