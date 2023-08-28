5eb77174756c37957cda1b7a7507e60c7dd4e78a
Alesandro <105898363+Alesandr0@users.noreply.github.com>
2023-08-28 08:45:36 -0300

    Implementação da Classe UsuarioService

--- Codigo/Core/Service/IUsuarioService.cs
+++ Codigo/Core/Service/IUsuarioService.cs
@@ -1,40 +1,15 @@
 ﻿using Core.DTO;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Core.Service
 {
-    internal interface IUsuarioService
+    public interface IUsuarioService
     {
-        int Inserir(Passageiro passageiro);
-
-        int Inserir(Motorista motorista);
-
-        void Alterar(Passageiro passageiro);
-
-        void Alterar(Motorista motorista);
-
-        Passageiro Buscar(int id);
-
-        //Motorista Buscar(int idPassageiro);
-
-        IEnumerable<Passageiro> ObterPorNome(string nome);
-
-        //IEnumerable<Motorista> ObterPorNome(string nome);
-
-        IEnumerable<Passageiro> ObterTodos();
-
-        //IEnumerable<Motorista> ObterTodos();
-
-        void Excluir(int idUsuario);
-
+        int Create(Passageiro passageiro);
+        Passageiro Get(int id);
+        void Edit(Passageiro passageiro);
+        void Delete(int id);
         IEnumerable<Passageiro> GetAll();
-
-        //IEnumerable<Motorista> GetAll();
-
-        IEnumerable<UsuarioDto> GetByNome(string nome);
+        IEnumerable<UsuarioDto> GetByPasssageiro(string nome);
     }
 }
+
