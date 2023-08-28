62db0ae4c3f2bb90780e5d3370a525a01f3727ed
Alesandro <105898363+Alesandr0@users.noreply.github.com>
2023-08-28 09:55:32 -0300

    Implementação da Classe UsuarioService

---
+++ Codigo/IUsuarioService.cs
@@ -0,0 +1,14 @@
+﻿using Core.DTO;
+
+namespace Core.Service
+{
+    public interface IUsuarioService
+    {
+        int Create(Passageiro passageiro);
+        Passageiro Get(int id);
+        void Edit(Passageiro passageiro);
+        void Delete(int id);
+        IEnumerable<Passageiro> GetAll();
+    }
+}
+
