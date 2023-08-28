12490c2ed878c3b28f4ba6a01c7a3bf23f555f1c
Alesandro <105898363+Alesandr0@users.noreply.github.com>
2023-08-28 10:47:33 -0300

    Implementação da Classe UsuarioService

---
+++ Codigo/Service/UsuarioService.cs
@@ -0,0 +1,90 @@
+﻿using Core;
+using Core.DTO;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+
+namespace Service
+{
+    /// <summary>
+    /// Manter dados de Passageiro no banco de dados
+    /// </summary>
+    public class UsuarioService : IUsuarioService
+    {
+        private readonly MaisTransporteContext _context;
+
+        public UsuarioService(MaisTransporteContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Insere um novo passageiro na base de dados
+        /// </summary>
+        /// <param name="passageiro">dados do usuario</param>
+        /// <returns></returns>
+        public int Create(Passageiro passageiro)
+        {
+            _context.Add(passageiro);
+            _context.SaveChanges();
+            return passageiro.Id;
+        }
+
+        public Passageiro Get(int id)
+        {
+            return _context.Passageiro.Find(id);
+        }
+
+        /// <summary>
+        /// Obtém todos os passageiros
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Passageiro> GetAll()
+        {
+            return _context.Passageiros;
+        }
+
+        /// <summary>
+        /// Remover o passageiro da base de dados
+        /// </summary>
+        /// <param name="id">id do passageiro</param>
+        public void Delete(int id)
+        {
+            var _passageiro = _context.Passageiros.Find(id);
+            _context.Remove(_passageiro);
+            _context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Editar dados do passageiro na base de dados
+        /// </summary>
+        /// <param name="passageiro"></param>
+        /// <exception cref="ServiceException"></exception>
+        public void Edit(Passageiro passageiro)
+        {
+            if (passageiro.dataNascimento.Year < 1000)
+                throw new ServiceException("O ano de nascimento de autor deve ser maior do que 1000. Favor informar nova data.");
+
+            _context.Update(passageiro);
+            _context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Obtém passageiros pelo nome
+        /// </summary>
+        /// <param name="nome">Nome a ser buscado</param>
+        /// <returns></returns>
+        public IEnumerable<UsuarioDto> GetByNome(string Nome)
+        {
+            IQueryable<Passageiro> tb_passageiro = _context.Passageiros;
+            var query = from nome in tb_passageiro
+                        where Nome.StartsWith(Nome)
+                        orderby Passageiro.Nome descending
+                        select new UsuarioDto
+                        {
+                            Nome = Passageiro.Nome
+                        };
+            return query;
+        }
+
+    }
+}
