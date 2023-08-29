32bc46bcae135ed1b4345bd9cfc5a11b484d6cef
gilmario <gilmariosantos1@gmail.com>
2023-08-28 22:20:23 -0300

    Implementação interface IVeiculoService e o Dto VeiculoDto

---
+++ Codigo/Core/Service/IVeiculoService.cs
@@ -0,0 +1,18 @@
+﻿using Core.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    internal interface IVeiculoService
+    {
+        int Create(Veiculo veiculo);
+        Veiculo Get(int id);
+        void Edit(Veiculo veiculo);
+        void Delete(int id);
+        IEnumerable<Veiculo> GetAll();
+    }
+}
