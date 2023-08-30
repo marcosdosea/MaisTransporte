1f8ed3360036ec2b153515aa80452b06ddf3d515
Alesandro <105898363+Alesandr0@users.noreply.github.com>
2023-08-30 15:07:23 -0300

    Implementação da Classe UsuarioViewModel

---
+++ Codigo/MaisTransporteWeb/Models/UsuarioViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MaisTransporteWeb.Models
+{
+    public class UsuarioViewModel
+    {
+        [Display(Name = "E-mail")]
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [Display(Name = "Telefone")]
+        [Required]
+        public string Telefone { get; set; } = null!;
+
+        [Display(Name = "Nome")]
+        [Required]
+        public string Nome { get; set; } = null!;
+
+        [Display(Name = "Data de Nascimento")]
+        [Required]
+        public DateTime DataNascimento { get; set; }
+        [DataType(DataType.Date)]
+
+        [Display(Name = "Cpf")]
+        [Required]
+        public string Cpf { get; set; } = null!;
+    }
+}
