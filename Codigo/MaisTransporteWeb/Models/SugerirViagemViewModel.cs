1a76bfa80a0d0b154051a4c27712bda1e22cfea3
Natalia Costa <102239679+nataliascosta@users.noreply.github.com>
2023-09-01 19:20:03 -0300

    Correção da classe SugerirViagemViewModel

--- Codigo/MaisTransporteWeb/Models/SugerirViagemViewModel.cs
+++ Codigo/MaisTransporteWeb/Models/SugerirViagemViewModel.cs
@@ -5,54 +5,54 @@ namespace MaisTransporteWeb.Models
     public class SugerirViagemViewModel
     {
         [Display(Name = "Código")]
-        [Required]
+        [Required(ErrorMessage = "Código da viagem é obrigatório."]
         public int Id { get; set; }
 
+        [Display(Name = "Código do Passageiro")]
+        [Required(ErrorMessage = "Código do passageiro é obrigatório."]
+        public int IdPassageiro { get; set; }
+
         [Display(Name = "Título")]
-        [Required]
+        [Required(ErrorMessage = "Título da viagem é obrigatório."]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Título da viagem deve ter entre 5 e 50 caracteres")]
         public string Titulo { get; set; } = null!;
 
         [Display(Name = "Local de origem")]
-        [Required]
+        [Required(ErrorMessage = "Local de origem da viagem é obrigatório."]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Local de origem deve ter entre 5 e 50 caracteres")]
         public string LocalOrigem { get; set; } = null!;
 
         [Display(Name = "Destino")]
-        [Required]
+        [Required(ErrorMessage = "Local de destino da viagem é obrigatório."]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "O destino deve ter entre 5 e 50 caracteres")]
         public string LocalDestino { get; set; } = null!;
 
         [Display(Name = "Valor da passagem")]
-        [Required]
+        [Required(ErrorMessage = "Valor da passagem é obrigatório."]
         public float ValorPassagem { get; set; }
 
         [Display(Name = "Total de vagas")]
-        [Required]
+        [Required(ErrorMessage = "Total de vagas da viagem é obrigatório."]
         public int TotalVagas { get; set; }
 
-        [Display(Name = "Data de partida")]
-        [Required]
+        [Display(Name = "Data e Horário de partida")]
+        [DataType(DataType.DateTime, ErrorMessage ="Data válida requerida"))]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DataPartida { get; set; }
-        [DataType(DataType.Date)]
 
-        [Display(Name = "Data de volta")]
-        [Required]
+        [Display(Name = "Data e Horário de volta")]
+        [DataType(DataType.DateTime, ErrorMessage ="Data válida requerida"))]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DataChegada { get; set; }
-        [DataType(DataType.Date)]
-
-        [Display(Name = "Horário 1")]
-        [Required]
-        public TimeSpan HorarioPartida { get; set; }
-
-        [Display(Name = "Horário 2")]
-        [Required]
-        public TimeSpan HorarioChegada { get; set; }
 
         [Display(Name = "Descrição")]
-        [Required]
+        [Required(ErrorMessage = "Descrição da viagem é obrigatória."]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "A descrição deve ter entre 10 e 100 caracteres")]
         public string Descricao { get; set; } = null!;
 
-        [Display(Name = "Status")]
-        [Required]
-        public string Status { get; set; } = null!;
+        [Display(Name = "Visibilidade")]
+        [Required(ErrorMessage = "Visibilidade da viagem é obrigatória."]
+        public string Visibilidade { get; set; } = null!;
     }
 }
 
