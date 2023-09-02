116866a6bb3852d1da615fc8ec695fbd8ff1c3d5
gilmario <gilmariosantos1@gmail.com>
2023-09-02 10:00:02 -0300

    Correção das máscaras dos campos obrigatórios da VeiculoViewModel.

--- Codigo/MaisTransporteWeb/Models/VeiculoViewModel.cs
+++ Codigo/MaisTransporteWeb/Models/VeiculoViewModel.cs
@@ -9,25 +9,30 @@ namespace MaisTransporteWeb.Models
         [Required]
         public int Id { get; set; }
 
-        [Display(Name = "Renavan")]
-        [Required]
+        [Display(Name = "Renavam")]
+        [Required(ErrorMessage = "Código renavam é obrigatório.")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "O código renavam deve ter 11 caracteres.")]
         public string Renavan { get; set; }
 
         [Display(Name = "Placa")]
-        [Required]
+        [Required(ErrorMessage = "Número da placa é obrigatório.")]
+        [StringLength(7, MinimumLength = 7, ErrorMessage = "O número da placa deve ter 7 caracteres.")]
         public string Placa { get; set; }
 
         [Display(Name = "DataEmissao")]
-        [Required]
+        [Required(ErrorMessage = "Data de emissão é obrigatório.")]
+        [DisplayFormat(DataFormatString = "{dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DataEmissao { get; set; }
         [DataType(DataType.Date)]
 
         [Display(Name = "Expedidor")]
-        [Required]
+        [Required(ErrorMessage = "Órgão expedidor é obrigatório.")]
+        [StringLength(3, MinimumLength = 5, ErrorMessage = "Órgão expedidor deve ter entre 3 e 5 caracteres.")]
         public string Expedidor { get; set; }
 
         [Display(Name = "Estado")]
-        [Required]
+        [Required(ErrorMessage = "O campo Estado é obrigatório.")]
+        [StringLength(2, MinimumLength = 2, ErrorMessage = "Escolha um Estado.")]
         public string Estado { get; set; }
 
 
