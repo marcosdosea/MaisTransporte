42cf7c5a8539a4226e95d664c1717a3b7140d2f9
gilmario <gilmariosantos1@gmail.com>
2023-09-02 11:00:04 -0300

    Correção nos campos de validação VeiculoViewModel.

--- Codigo/MaisTransporteWeb/Models/VeiculoViewModel.cs
+++ Codigo/MaisTransporteWeb/Models/VeiculoViewModel.cs
@@ -4,12 +4,11 @@ namespace MaisTransporteWeb.Models
 {
     public class VeiculoViewModel
     {
-
-        [Display(Name = "Código")]
-        [Required]
+                
+        [Key]
         public int Id { get; set; }
 
-        [Display(Name = "Renavam")]
+        [Display(Name = "Código Renavam")]
         [Required(ErrorMessage = "Código renavam é obrigatório.")]
         [StringLength(11, MinimumLength = 11, ErrorMessage = "O código renavam deve ter 11 caracteres.")]
         public string Renavan { get; set; }
@@ -19,13 +18,14 @@ namespace MaisTransporteWeb.Models
         [StringLength(7, MinimumLength = 7, ErrorMessage = "O número da placa deve ter 7 caracteres.")]
         public string Placa { get; set; }
 
-        [Display(Name = "DataEmissao")]
+        
+        [Display(Name = "Data de Emissão")]
+        [DataType(DataType.DateTime, ErrorMessage = "Data válida requerida")]
         [Required(ErrorMessage = "Data de emissão é obrigatório.")]
         [DisplayFormat(DataFormatString = "{dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DataEmissao { get; set; }
-        [DataType(DataType.Date)]
-
-        [Display(Name = "Expedidor")]
+        
+        [Display(Name = "Órgão Expedidor")]
         [Required(ErrorMessage = "Órgão expedidor é obrigatório.")]
         [StringLength(3, MinimumLength = 5, ErrorMessage = "Órgão expedidor deve ter entre 3 e 5 caracteres.")]
         public string Expedidor { get; set; }
