8d12610c56c2a0af059d78c759ee16b2a3402c07
gilmario <gilmariosantos1@gmail.com>
2023-09-02 02:12:25 -0300

    Implementação da classe VeiculoViewModel.

---
+++ Codigo/MaisTransporteWeb/Models/VeiculoViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MaisTransporteWeb.Models
+{
+    public class VeiculoViewModel
+    {
+
+        [Display(Name = "Código")]
+        [Required]
+        public int Id { get; set; }
+
+        [Display(Name = "Renavan")]
+        [Required]
+        public string Renavan { get; set; }
+
+        [Display(Name = "Placa")]
+        [Required]
+        public string Placa { get; set; }
+
+        [Display(Name = "DataEmissao")]
+        [Required]
+        public DateTime DataEmissao { get; set; }
+        [DataType(DataType.Date)]
+
+        [Display(Name = "Expedidor")]
+        [Required]
+        public string Expedidor { get; set; }
+
+        [Display(Name = "Estado")]
+        [Required]
+        public string Estado { get; set; }
+
+
+    }
+}
