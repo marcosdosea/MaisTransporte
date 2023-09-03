1b797c056aff9bd500ef55ca9173b387ae785f6d
Alesandro <105898363+Alesandr0@users.noreply.github.com>
2023-09-03 18:56:41 -0300

    Implementação com as devidas alterações MotoristaViewModel.cs

--- Codigo/MaisTransporteWeb/Models/MotoristaViewModel.cs
+++ Codigo/MaisTransporteWeb/Models/MotoristaViewModel.cs
@@ -5,20 +5,24 @@ namespace MaisTransporteWeb.Models
     public class MotoristaViewModel
     {
         [Display(Name = "Nº do Documento")]
-        [Required]
+        [Required(ErrorMessage = "Número do documento é obrigatório.")]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "Número do documento deve ter entre 5 e 20 caracteres.")]
         public string NumeroDocumento { get; set; } = null!;
 
         [DataType(DataType.Date)]
         [Display(Name = "Data de Emissão")]
-        [Required]
+        [Required(ErrorMessage = "A data de Emissão é obrigatória.")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DataEmissao { get; set; }
-
+      
         [Display(Name = "Órgão Expeditor")]
-        [Required]
+        [Required(ErrorMessage = "O nome do órgão expeditor é obrigatório.")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Órgão Expeditor deve ter entre 5 e 50 caracteres.")]
         public string OrgaoExpeditor { get; set; } = null!;
 
         [Display(Name = "Estado")]
-        [Required]
+        [Required(ErrorMessage = "O nome do Estado é obrigatório.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Estado deve ter entre 3 e 50 caracteres.")]
         public string Estado { get; set; } = null!;
     }
 }
