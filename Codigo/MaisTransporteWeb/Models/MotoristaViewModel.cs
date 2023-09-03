b6cbf09ce68fe4d249cfb4f6e7331f0a08377ed2
Alesandro <105898363+Alesandr0@users.noreply.github.com>
2023-09-03 17:52:16 -0300

    Implementação da classe MotoristaViewModel

---
+++ Codigo/MaisTransporteWeb/Models/MotoristaViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MaisTransporteWeb.Models
+{
+    public class MotoristaViewModel
+    {
+        [Display(Name = "Nº do Documento")]
+        [Required]
+        public string NumeroDocumento { get; set; } = null!;
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Data de Emissão")]
+        [Required]
+        public DateTime DataEmissao { get; set; }
+
+        [Display(Name = "Órgão Expeditor")]
+        [Required]
+        public string OrgaoExpeditor { get; set; } = null!;
+
+        [Display(Name = "Estado")]
+        [Required]
+        public string Estado { get; set; } = null!;
+    }
+}
