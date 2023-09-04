58f307c8eddcba7edb9eb2be1bd597863a67e2c1
Alesandro <105898363+Alesandr0@users.noreply.github.com>
2023-09-03 22:26:47 -0300

    Implementação da classe MotoristaProfile(Mappers)

---
+++ Codigo/MaisTransporteWeb/Mappers/MotoristaProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core;
+using MaisTransporteWeb.Models;
+
+namespace MaisTransporteWeb.Mappers
+{
+    public class MotoristaProfile : Profile
+    {
+        public MotoristaProfile()
+        {
+            CreateMap<MotoristaViewModel, Passageiro>().ReverseMap();
+        }
+    }
+}
