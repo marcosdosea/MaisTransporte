9cedcc7f82facdeb0da646823892e15fa3a7cb6f
Alesandro <105898363+Alesandr0@users.noreply.github.com>
2023-09-03 22:01:37 -0300

    Alteração da classe MotoristaViewModel.cs conforme especificado

--- Codigo/MaisTransporteWeb/Models/MotoristaViewModel.cs
+++ Codigo/MaisTransporteWeb/Models/MotoristaViewModel.cs
@@ -6,7 +6,7 @@ namespace MaisTransporteWeb.Models
     {
         [Display(Name = "Nº do Documento")]
         [Required(ErrorMessage = "Número do documento é obrigatório.")]
-        [StringLength(20, MinimumLength = 5, ErrorMessage = "Número do documento deve ter entre 5 e 20 caracteres.")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Número do documento deve ter entre 5 e 15 caracteres.")]
         public string NumeroDocumento { get; set; } = null!;
 
         [DataType(DataType.Date)]
@@ -17,12 +17,12 @@ namespace MaisTransporteWeb.Models
       
         [Display(Name = "Órgão Expeditor")]
         [Required(ErrorMessage = "O nome do órgão expeditor é obrigatório.")]
-        [StringLength(50, MinimumLength = 5, ErrorMessage = "Órgão Expeditor deve ter entre 5 e 50 caracteres.")]
+        [StringLength(5, MinimumLength = 2, ErrorMessage = "Órgão Expeditor deve ter entre 2 e 50 caracteres.")]
         public string OrgaoExpeditor { get; set; } = null!;
 
         [Display(Name = "Estado")]
         [Required(ErrorMessage = "O nome do Estado é obrigatório.")]
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "Estado deve ter entre 3 e 50 caracteres.")]
+        [StringLength(2, MinimumLength = 1, ErrorMessage = "Estado deve ter entre 3 e 50 caracteres.")]
         public string Estado { get; set; } = null!;
     }
 }
