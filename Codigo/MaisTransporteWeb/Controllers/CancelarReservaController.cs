3b7b3b4d39974b8bbbe36b91f1fb68d14c0e3a77
Natalia Costa <102239679+nataliascosta@users.noreply.github.com>
2023-09-05 09:09:34 -0300

    Criação da classe CancelarReservaController. Issue #227

---
+++ Codigo/MaisTransporteWeb/Controllers/CancelarReservaController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Core;
+using Core.Service;
+using MaisTransporteWeb.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service;
+
+namespace MaisTransporteWeb.Controllers
+{
+    public class CancelarReservaController : Controller
+    {
+        private readonly IReservaService reservaService;
+        private readonly IMapper mapper;
+
+        public CancelarReservaController(IReservaService reservaService, IMapper mapper)
+        {
+            this.reservaService = reservaService;
+            this.mapper = mapper;
+        }
+
+        // GET: CancelarReservaController
+        public ActionResult Index()
+        {
+            var listaReservas = reservaService.GetAll();
+            var listaReservasViewModel = mapper.Map<List<ReservaViewModel>>(listaReservas);
+            return View(listaReservasViewModel);
+        }
+
+        // GET: CancelarReservaController/Delete
+        public ActionResult Delete(int id)
+        {
+            Reserva reserva = reservaService.Get(id);
+            ReservaViewModel reservaViewModel = mapper.Map<ReservaViewModel>(reserva);
+            return View(reservaViewModel);
+        }
+
+        // POST: CancelarReservaController/Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, ReservaViewModel reservaModel)
+        {
+            reservaService.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
