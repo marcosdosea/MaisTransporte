7a4c9b0baa7acae27b0208297d3da7609483862a
Eliane Dantas <edantas241@gmail.com>
2023-09-04 22:13:04 -0300

    Criação da interface de Reservar Vaga

---
+++ Codigo/Core/Service/IReservaService.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Service
+{
+    public interface IReservaService
+    {
+        int Create(Reserva reserva);
+        Reserva Get(int id);
+        void Delete(int id);
+        IEnumerable<Reserva> GetAll();
+    }
+}
