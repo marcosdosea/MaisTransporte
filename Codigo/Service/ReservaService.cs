b66d3c2914609de158eee921c44f083768c15cbe
Eliane Dantas <edantas241@gmail.com>
2023-09-04 22:19:00 -0300

    Implementação da classe Service de Reserva

---
+++ Codigo/Service/ReservaService.cs
@@ -0,0 +1,39 @@
+﻿using Core;
+using Core.Service;
+
+namespace Service
+{
+    public class ReservaService : IReservaService
+    {
+        private readonly MaisTransporteContext _context;
+
+        public ReservaService(MaisTransporteContext context)
+        {
+            _context = context;
+        }
+
+        public int Create(Reserva reserva)
+        {
+            _context.Add(reserva);
+            _context.SaveChanges();
+            return reserva.Id;
+        }
+
+        public Reserva Get(int id)
+        {
+            return _context.Reservas.Find(id);
+        }
+
+        public void Delete(int id)
+        {
+            var reserva = _context.Reservas.Find(id);
+            _context.Remove(reserva);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Reserva> GetAll()
+        {
+            return _context.Reservas;
+        }
+    }
+}
