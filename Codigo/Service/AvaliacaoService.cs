db8d05a332fe4de205983f17ffb24713ca6e30d6
gilmariosantos1 <gilmariosantos1@gmail.com>
2023-09-08 19:44:31 -0300

    Implementação da classe Avaliação Serviçe

---
+++ Codigo/Service/AvaliacaoService.cs
@@ -0,0 +1,56 @@
+﻿using Core;
+using Core.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    /// <summary>
+    /// Manter avaliação viagens no banco de dados
+    /// </summary>
+    /// 
+    public class AvaliarViagemService : IAvaliacaoViagemService
+    {
+        private readonly MaisTransporteContext _context;
+
+        public AvaliarViagemService(MaisTransporteContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Insere uma nova avaliação na base de dados
+        /// </summary>
+        /// <param name="avaliacaoViagem">dados da avaliação</param>
+        /// <returns></returns>
+        public int Create(Avaliacao avaliacao)
+        {
+            _context.Add(avaliacao);
+            _context.SaveChanges();
+            return avaliacao.Id;
+        }
+
+        /// <summary>
+        /// Obtém todas as avaliações das viagens
+        /// </summary>
+        /// <returns></returns>
+        public Avaliacao Get(int id)
+        {
+            return _context.Avaliacaos.Find(id);
+        }
+
+        
+        /// /// <summary>
+        /// Obtém todas avaliações das viagens
+        /// </summary>
+        /// <param name="avaliaViagem">avaliar viagem</param>
+        /// <returns></returns>
+        public IEnumerable<Avaliacao> GetAll()
+        {
+            return _context.Avaliacaos;
+        }
+    }
+}
