29d3b3ca2f9a874b673ba5887d392a18da9b5038
gilmario <gilmariosantos1@gmail.com>
2023-09-19 23:32:42 -0300

    Implementação do teste service e teste controller do caso de uso Manter Veículo

---
+++ Codigo/MaisTransporteWebTests/Controllers/VeiculoControllerTests.cs
@@ -0,0 +1,189 @@
+﻿using AutoMapper;
+using Core;
+using Core.Service;
+using MaisTransporteWeb.Controllers;
+using MaisTransporteWeb.Mappers;
+using MaisTransporteWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaisTransporteWebTests.Controllers
+{
+    [TestClass]
+    public class VeiculoControllerTests
+    {
+        private static VeiculoController controller;
+        //private IVeiculoService @object;
+        //private IMapper mapper;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var mockService = new Mock<IVeiculoService>();
+
+            IMapper mapper = new MapperConfiguration(cfg =>
+               cfg.AddProfile(new VeiculoProfile())).CreateMapper();
+
+            mockService.Setup(service => service.GetAll())
+               .Returns(GetTestVeiculo());
+            mockService.Setup(service => service.Get(1))
+               .Returns(GetTargetVeiculo());
+            mockService.Setup(service => service.Create(It.IsAny<Veiculo>()))
+               .Verifiable();
+            controller = new VeiculoController(mockService.Object, mapper);
+        }
+
+
+        [TestMethod()]
+        public void IndexTest()
+        {
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(List<VeiculoViewModel>));
+
+            List<VeiculoViewModel>? lista = (List<VeiculoViewModel>)viewResult.ViewData.Model;
+            Assert.AreEqual(2, lista.Count);
+        }
+
+
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            // Act
+            var result = controller.Details(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(VeiculoViewModel));
+            VeiculoViewModel veiculoViewModel = (VeiculoViewModel)viewResult.ViewData.Model;
+
+            Assert.AreEqual("Renavam", veiculoViewModel.Renavam);
+            Assert.AreEqual("Placa", veiculoViewModel.Placa);
+            Assert.AreEqual(DateTime.Parse("2022-08-02"), veiculoViewModel.DataEmissao);
+            Assert.AreEqual("Órgão Expedidor", veiculoViewModel.Expedidor);
+            Assert.AreEqual("Estado", veiculoViewModel.Estado);
+            Assert.AreEqual(1, veiculoViewModel.IdMotoristaPassageiro);
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            // Act
+            var result = controller.Create();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod()]
+        public void CreateTest_Valid()
+        {
+            // Act
+            var result = controller.Create(GetNewVeiculo());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+        [TestMethod()]
+        public void CreateTest_Invalid()
+        {
+            // Arrange
+            controller.ModelState.AddModelError("Renavam", "Número do Renavam é obrigatório.");
+
+            // Act
+            var result = controller.Create(GetNewVeiculo());
+
+            // Assert
+            Assert.AreEqual(1, controller.ModelState.ErrorCount);
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+        private VeiculoViewModel GetNewVeiculo()
+        {
+            return new VeiculoViewModel
+            {
+                Id = 1,
+                Renavam = "123235212",
+                Placa = "OHD-2929",
+                DataEmissao = DateTime.Parse("2023-04-10"),
+                Expedidor = "SSP",
+                Estado = "SE",
+                IdMotoristaPassageiro = 1,
+            };
+        }
+        
+        private Veiculo GetTargetVeiculo()
+        {
+            return new Veiculo
+            {
+                Id = 5,
+                Renavam = "123235212",
+                Placa = "OHD-2929",
+                DataEmissao = DateTime.Parse("2023-04-10"),
+                Expedidor = "SSP",
+                Estado = "SE",
+                IdMotoristaPassageiro = 1,
+            };
+        }
+        private VeiculoViewModel GetTargetVeiculoViewModel()
+        {
+            return new VeiculoViewModel
+            {
+                Id = 10,
+                Renavam = "123235212",
+                Placa = "OHD-2929",
+                DataEmissao = DateTime.Parse("2023-04-10"),
+                Expedidor = "SSP",
+                Estado = "SE",
+                IdMotoristaPassageiro = 1
+            };
+        }
+
+        private IEnumerable<Veiculo> GetTestVeiculo()
+        {
+            return new List<Veiculo>
+            {
+                new Veiculo
+                {
+                    Id = 1,
+                    Renavam = "123235212",
+                    Placa = "OHD-2929",
+                    DataEmissao = DateTime.Parse("2023-04-10"),
+                    Expedidor = "SSP",
+                    Estado = "SE",
+                    IdMotoristaPassageiro = 1
+                },
+                new Veiculo
+                {
+                    Id = 2,
+                    Renavam = "123235212",
+                    Placa = "OHD-2929",
+                    DataEmissao = DateTime.Parse("2023-04-10"),
+                    Expedidor = "SSP",
+                    Estado = "SE",
+                    IdMotoristaPassageiro = 1,
+                },
+            };
+        }
+
+
+    }
+}
---
+++ Codigo/ServiceTests/VeiculoServiceTests.cs
@@ -0,0 +1,87 @@
+﻿using Core.Service;
+using Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceTests
+{
+    [TestClass]
+    public class VeiculoServiceTests
+    {
+        private MaisTransporteContext _context;
+        private IVeiculoService _veiculoService;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            //Arrange
+            var builder = new DbContextOptionsBuilder<MaisTransporteContext>();
+            builder.UseInMemoryDatabase("ModeloMaisTransporte");
+            var options = builder.Options;
+
+            _context = new MaisTransporteContext(options);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+            var sugestoes = new List<Veiculo>
+                {
+                    new Veiculo {
+                        Id = 5,
+                        Renavam = "123235212",
+                        Placa = "OHD-2929",
+                        DataEmissao = DateTime.Parse("2023-04-10"),
+                        Expedidor = "SSP",
+                        Estado = "SE",
+                        IdMotoristaPassageiro = 1,
+                    }
+                };
+
+            _context.AddRange(sugestoes);
+            _context.SaveChanges();
+
+            _veiculoService = new VeiculoService(_context);
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            //Act
+            _veiculoService.Create(new Veiculo()
+            {
+                Id = 9,
+                Renavam = "123235212",
+                Placa = "OHD-2929",
+                DataEmissao = DateTime.Parse("2023-04-10"),
+                Expedidor = "SSP",
+                Estado = "SE",
+                IdMotoristaPassageiro = 1,
+            });
+                //Assert
+                Assert.AreEqual(2, _veiculoService.GetAll().Count());
+                var veiculo = _veiculoService.Get(2);
+                Assert.AreEqual("Renavam", veiculo.Renavam);
+                Assert.AreEqual("Placa", veiculo.Placa);
+                Assert.AreEqual(DateTime.Parse("2022-08-02"), veiculo.DataEmissao);
+                Assert.AreEqual("Órgão Expedidor", veiculo.Expedidor);
+                Assert.AreEqual("Estado", veiculo.Estado);
+                Assert.AreEqual(1, veiculo.IdMotoristaPassageiro);
+
+        }
+        public void GetTest()
+        {
+            var veiculo = _veiculoService.Get(1);
+            Assert.IsNotNull(veiculo);
+            Assert.AreEqual("Renavam", veiculo.Renavam);
+            Assert.AreEqual("Placa", veiculo.Placa);
+            Assert.AreEqual(DateTime.Parse("2022-08-02"), veiculo.DataEmissao);
+            Assert.AreEqual("Órgão Expedidor", veiculo.Expedidor);
+            Assert.AreEqual("Estado", veiculo.Estado);
+            Assert.AreEqual(1, veiculo.IdMotoristaPassageiro);
+        }
+    }
+}
