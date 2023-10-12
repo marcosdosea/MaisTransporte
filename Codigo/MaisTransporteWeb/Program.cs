1da2d574cfe10a3098a672929b61e31a924b04ac
Eliane Dantas <edantas241@gmail.com>
2023-10-12 13:14:17 -0300

    Implementação de autenticação de usuários

--- Codigo/MaisTransporteWeb/Program.cs
+++ Codigo/MaisTransporteWeb/Program.cs
@@ -2,6 +2,9 @@ using Core;
 using Core.Service;
 using Service;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using MaisTransporteWeb.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace MaisTransporteWeb
 {
@@ -14,18 +17,60 @@ namespace MaisTransporteWeb
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
-
-            builder.Services.AddDbContext<MaisTransporteContext>(
-                options => options.UseMySQL(builder.Configuration.GetConnectionString("MaisTransporteDatabase")));
-
-            //injeção dependência Services
+            //injeï¿½ï¿½o dependï¿½ncia Services
             builder.Services.AddTransient<IViagemService, ViagemService>();
             builder.Services.AddTransient<ISugerirViagemService, SugerirViagemService>();
             builder.Services.AddTransient<IUsuarioService, UsuarioService>();
             builder.Services.AddTransient<IVeiculoService, VeiculoService>();
+            builder.Services.AddTransient<IMotoristaService, MotoristaService>();
 
-            // injeção dependência mappers
+            // injeï¿½ï¿½o dependï¿½ncia mappers
             builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
+            builder.Services.AddDbContext<MaisTransporteContext>(
+                options => options.UseMySQL(builder.Configuration.GetConnectionString("MaisTransporteDatabase")));
+
+            builder.Services.AddDbContext<IdentityContext>(
+                options => options.UseMySQL(builder.Configuration.GetConnectionString("MaisTransporteDatabase")));
+
+            builder.Services.AddDefaultIdentity<UsuarioIdentity>(options => 
+            {
+                // SignIn settings
+                options.SignIn.RequireConfirmedAccount = false;
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+
+                // Password settings
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+
+                // Default User settings.
+                options.User.AllowedUserNameCharacters =
+                        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = false;
+
+                // Default Lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+            }).AddRoles<IdentityRole>()
+              .AddEntityFrameworkStores<IdentityContext>();
+
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+                options.Cookie.Name = "YourAppCookieName";
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+                options.LoginPath = "/Identity/Account/Login";
+                // ReturnUrlParameter requires 
+                options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
+                options.SlidingExpiration = true;
+            });
 
             var app = builder.Build();
 
@@ -40,9 +85,11 @@ namespace MaisTransporteWeb
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
-            app.UseRouting();
+            app.UseRouting();                      
+            app.UseAuthentication();;
 
             app.UseAuthorization();
+            app.MapRazorPages();
 
             app.MapControllerRoute(
                 name: "default",
