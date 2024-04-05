eb9e44290a2c69642dfff7016519250133b35fdf
Eliane Dantas <edantas241@gmail.com>
2024-04-05 19:05:50 -0300

    Atualização da tela de login

--- Codigo/MaisTransporteWeb/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Codigo/MaisTransporteWeb/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -81,7 +81,7 @@ namespace MaisTransporteWeb.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Manter conectado?")]
             public bool RememberMe { get; set; }
         }
 
