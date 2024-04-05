f44707cc32d7fd0923515e4f317f17e56f614815
Eliane Dantas <edantas241@gmail.com>
2024-04-05 19:35:04 -0300

    Atualização da tela de cadastro

--- Codigo/MaisTransporteWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Codigo/MaisTransporteWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -77,7 +77,7 @@ namespace MaisTransporteWeb.Areas.Identity.Pages.Account
             /// </summary>
             [Required]
             [EmailAddress]
-            [Display(Name = "Email")]
+            [Display(Name = "E-mail")]
             public string Email { get; set; }
 
             /// <summary>
@@ -85,9 +85,9 @@ namespace MaisTransporteWeb.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "A {0} deve ter pelo menos {2} e no máximo {1} caracteres.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
+            [Display(Name = "Senha")]
             public string Password { get; set; }
 
             /// <summary>
@@ -95,8 +95,8 @@ namespace MaisTransporteWeb.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Display(Name = "Confirmar senha")]
+            [Compare("Password", ErrorMessage = "A senha e a senha de confirmação não coincidem.")]
             public string ConfirmPassword { get; set; }
         }
 
