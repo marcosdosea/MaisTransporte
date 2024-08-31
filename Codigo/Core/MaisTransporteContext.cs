bc5f1cae8dd62fb6c1e6b2e8f14aca17e7a0cf78
Natalia Costa <102239679+nataliascosta@users.noreply.github.com>
2024-08-31 10:23:33 -0300

    Correção do context com a adição de comentário. #291

--- Codigo/Core/MaisTransporteContext.cs
+++ Codigo/Core/MaisTransporteContext.cs
@@ -45,9 +45,9 @@ public partial class MaisTransporteContext : DbContext
 
     public virtual DbSet<Viagem> Viagems { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySQL("server=localhost;port=3306;user=root;password=123456;database=ModeloMaisTransporte");
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseMySQL("server=localhost;port=3306;user=root;password=123456;database=ModeloMaisTransporte");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
