a11f64b46a37c9755bbfc2259b9dedeecc72e689
Natalia Costa <102239679+nataliascosta@users.noreply.github.com>
2024-09-02 16:24:57 -0300

    Implementação de método para excluir a sugestão de viagem quando a mesma é aceita. #293

--- Codigo/Core/Service/ISugerirViagemService.cs
+++ Codigo/Core/Service/ISugerirViagemService.cs
@@ -5,8 +5,8 @@ namespace Core.Service
     public interface ISugerirViagemService
     {
         int Create(Sugestaoviagem sugestaoViagem);
+        void Delete(int id);
         Sugestaoviagem Get(int id);
-
         IEnumerable<Sugestaoviagem> GetAll();
         IEnumerable<SugerirViagemDto> GetByLocalDestino(string localDestino);
     }
